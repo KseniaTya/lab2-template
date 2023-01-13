f6ae2c31372a05bbdeebe455529766e71fe6a79d
Ksenia <ezik1miliy@gmail.com>
2023-01-13 20:46:35 +0300

    Поправлено добавление резервации

--- services/gateway_service/src/post_reservations.php
+++ services/gateway_service/src/post_reservations.php
@@ -8,8 +8,10 @@
     $username= getallheaders()['X-User-Name'] ?? "Test_User";
     $numBooks = curl("http://reservation_system:80/num_books?username=$username");
     $numStars = curl("http://rating_system:80/num_stars?username=$username");
+    $available_count  = curl("http://library_system:80/getBook?book_uid=$bookUid&library_uid=$libraryUid");
+    echo "available_count = $available_count ";
     //echo "tillDate = $tillDate numBooks = $numBooks numStars = $numStars";
-    if($numBooks < $numStars){
+    if($numBooks < $numStars && $available_count > 0){
         // процесс взятия книги
         $p = curl("http://reservation_system:80/add_reserv?username=$username&book_uid=$bookUid&library_uid=$libraryUid&till_date=$tillDate");
         $f = curl("http://library_system:80/minus_book?book_uid=$bookUid&library_uid=$libraryUid");
--- services/library_system/routes.php
+++ services/library_system/routes.php
@@ -12,3 +12,4 @@ get('/manage/health', 'src/health.php');
 get('/get_libraries', "src/get_libraries.php");
 get('/get_books', "src/get_books.php");
 get('/minus_book', "src/minus_book.php");
+get('/getBook', "src/getBook.php");
