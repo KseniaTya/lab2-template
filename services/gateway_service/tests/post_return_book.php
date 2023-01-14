ddaede76a9796dc0603fe2c703ab8b91c06dd590
KseniaTya <56265098+KseniaTya@users.noreply.github.com>
2023-01-14 19:48:22 +0300

    Проверка push GitHub.

--- services/gateway_service/tests/post_return_book.php
+++ services/gateway_service/tests/post_return_book.php
@@ -6,3 +6,4 @@ $reservation_uid = $arr[0]->reservation_uid;
 echo curl_post("http://gateway_service:80/api/v1/reservations/$reservation_uid/return",
     "condition=EXCELLENT&date=2021-10-11"
     ,['X-User-Name: ksenia']);
+
