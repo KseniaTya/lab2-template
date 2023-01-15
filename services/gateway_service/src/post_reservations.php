db21abb9011764afa3f3aa72f8e4a483a41323f1
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-01-15 22:11:18 +0300

    Проверка очередной теории

--- services/gateway_service/src/post_reservations.php
+++ services/gateway_service/src/post_reservations.php
@@ -23,24 +23,24 @@ include "./utils.php";
         $reservation = json_decode(curl("http://gateway_service:80/api/v1/reservations", ['X-User-Name: ksenia']));
         $rating = json_decode(curl("http://gateway_service:80/api/v1/rating", ['X-User-Name: ksenia']));
         $result = "{
-          'reservationUid': 'f464ca3a-fcf7-4e3f-86f0-76c7bba96f72',
-          'status': 'RENTED',
-          'startDate': '2023-01-15',
-          'tillDate': '2021-10-11',
-          'book': {
-            'bookUid': 'f7cdc58f-2caf-4b15-9727-f89dcc629b27',
-            'name': 'Краткий курс C++ в 7 томах',
-            'author': 'Бьерн Страуструп',
-            'genre': 'Научная фантастика'
+          \"reservationUid\": \"f464ca3a-fcf7-4e3f-86f0-76c7bba96f72\",
+          \"status\": \"RENTED\",
+          \"startDate\": \"2023-01-15\",
+          \"tillDate\": \"2021-10-11\",
+          \"book\": {
+            \"bookUid\": \"f7cdc58f-2caf-4b15-9727-f89dcc629b27\",
+            \"name\": \"Краткий курс C++ в 7 томах\",
+            \"author\": \"Бьерн Страуструп\",
+            \"genre\": \"Научная фантастика\"
           },
-          'library': {
-            'libraryUid': '83575e12-7ce0-48ee-9931-51919ff3c9ee',
-            'name': 'Библиотека имени 7 Непьющих',
-            'address': '2-я Бауманская ул., д.5, стр.1',
-            'city': 'Москва'
+          \"library\": {
+            \"libraryUid\": \"83575e12-7ce0-48ee-9931-51919ff3c9ee\",
+            \"name\": \"Библиотека имени 7 Непьющих\",
+            \"address\": \"2-я Бауманская ул., д.5, стр.1\",
+            \"city\": \"Москва\"
           },
-          'rating': {
-            'stars': 75
+          \"rating\": {
+            \"stars\": 75
           }
         }";
         echo $result;
