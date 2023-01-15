c74a856a51142fe7b173adcca093f4521123e112
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-01-15 15:14:26 +0300

    Добавлен более грамотный вывод ошибок при неудачном создании резервации

--- services/gateway_service/src/post_reservations.php
+++ services/gateway_service/src/post_reservations.php
@@ -24,7 +24,7 @@ include "./utils.php";
         echo json_encode($result);
 
     }else{
-        echo "numBooks > numStars or available_count == 0 ";
+        echo '{"message" : "numBooks > numStars or available_count == 0"';
     }
 
     function validate($array, $func, $err_code){
