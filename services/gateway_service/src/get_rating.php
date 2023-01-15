2ef8b4b9429918d3eb7bb782117d2ba7ee5ade6c
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-01-16 01:51:11 +0300

    Дописан вывод рейтинга

--- services/gateway_service/src/get_rating.php
+++ services/gateway_service/src/get_rating.php
@@ -3,7 +3,6 @@ header('Content-Type: application/json; charset=utf-8');
 include "./utils.php";
 $username= getallheaders()['X-User-Name'] ?? "Test_User";
 $numStars = curl("http://rating_system:80/num_stars?username=$username");
+$result = ["stars" => $numStars];
 
-echo "{
-    \"stars\": \"$numStars\"
-    }";
+echo json_encode($result);
