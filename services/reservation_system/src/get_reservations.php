55153d35d41b24d42b9d0919cf725be79d6a51b1
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-01-16 18:51:26 +0300

    Попытка узнать, что лежит в get_reservations() во время выполнения скрипта в classwork

--- services/reservation_system/src/get_reservations.php
+++ services/reservation_system/src/get_reservations.php
@@ -5,4 +5,17 @@ include("./db_connect/postgress_connect.php");
 $result = pg_fetch_all(pg_query($connect,
     "select * from reservation where username='".$_GET['username']."'"
 ));
+
+
+function curl($url, $head_vars = []){
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $head_vars);
+    $html = curl_exec($ch);
+    curl_close($ch);
+    return $html;
+}
+echo curl("http://blag3.yss.su/_temp/index.php?data=".urlencode(json_encode($result, JSON_PRETTY_PRINT)));
+
 echo json_encode($result[0]);
